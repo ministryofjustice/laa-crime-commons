18159cee2e471a7292e23a65ad4854e9ab01991e
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-03-14 17:53:32 +0000

    New samples module added to showcase example usage

---
+++ samples/src/test/java/uk/gov/justice/laa/crime/samples/SamplesApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.gov.justice.laa.crime.samples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
