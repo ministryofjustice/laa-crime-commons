41d1efb95c0190e7add905ac7758ccb806b71038
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-03-27 18:33:11 +0100

    Make branches require approval before triggering a build

--- samples/src/test/java/uk/gov/justice/laa/crime/samples/SamplesApplicationTests.java
+++
@@ -1,9 +0,0 @@
-package uk.gov.justice.laa.crime.samples;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class SamplesApplicationTests {
-
-}
