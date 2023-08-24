ff7fb8236883b01ec909bba6d4186386d9f0dc64
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-08-24 14:17:03 +0100

    Autoconfiguration of beans to support WebClient usage in non-servlet environments.

--- crime-commons-spring-boot-starter-rest-client/src/main/java/uk/gov/justice/laa/crime/commons/config/RestClientAutoConfiguration.java
+++ crime-commons-spring-boot-starter-rest-client/src/main/java/uk/gov/justice/laa/crime/commons/config/RestClientAutoConfiguration.java
@@ -6,7 +6,6 @@ import org.eclipse.jetty.client.HttpClient;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnExpression;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
@@ -65,7 +64,10 @@ public class RestClientAutoConfiguration {
 
             webClientBuilder.defaultHeader(HttpHeaders.ACCEPT, MediaType.APPLICATION_JSON_VALUE);
             webClientBuilder.defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE);
-            webClientBuilder.codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(Constants.MAX_IN_MEMORY_SIZE));
+            webClientBuilder.codecs(configurer -> configurer
+                    .defaultCodecs()
+                    .maxInMemorySize(Constants.MAX_IN_MEMORY_SIZE)
+            );
 
             webClientBuilder.filters(filters -> {
                 filters.add(WebClientFilters.logResponse());
@@ -76,8 +78,17 @@ public class RestClientAutoConfiguration {
         };
     }
 
+    /**
+     * Configures an <code>OAuth2AuthorizedClientManager</code> of type <code>AuthorizedClientServiceOAuth2AuthorizedClientManager</code> for use in non-servlet environments (e.g. SQS listeners, scheduled tasks)
+     * Instantiation is conditional on the configuration of Spring Cloud access credentials
+     *
+     * @param clientService                the client service
+     * @param clientRegistrationRepository the client registration repository
+     * @return the OAuth 2 authorized client manager
+     */
     @Bean
     @Order(2)
+    @ConditionalOnProperty(name = "spring.cloud.aws.credentials.access-key")
     public OAuth2AuthorizedClientManager clientServiceAuthorizedClientManager(
             OAuth2AuthorizedClientService clientService, ClientRegistrationRepository clientRegistrationRepository) {
 
@@ -107,12 +118,13 @@ public class RestClientAutoConfiguration {
     }
 
     /**
-     * Configures a WebClient bean via a builder instance
-     * Configuration is inherited from <code>WebClientCustomizer</code>
+     * Configures a WebClient bean with a <code>ServletOAuth2AuthorizedClientExchangeFilterFunction</code> via its builder.
+     * Configuration is inherited from <code>WebClientCustomizer</code>. An OAuth2 filter is added here for authentication purposes
      *
      * @param builder the builder
      * @return the web client
      * @see WebClientCustomizer
+     * @see ServletOAuth2AuthorizedClientExchangeFilterFunction
      */
     @Bean
     @Primary
@@ -127,10 +139,22 @@ public class RestClientAutoConfiguration {
         return builder.build();
     }
 
+    /**
+     * Configures a WebClient bean with a <code>ServletOAuth2AuthorizedClientExchangeFilterFunction</code> via its builder for use in non-servlet environments
+     * (e.g. SQS listeners, scheduled tasks).
+     * Configuration is inherited from <code>WebClientCustomizer</code>. Instantiation is conditional on the presence of a <code>OAuth2AuthorizedClientManager</code> bean with a specific qualifier
+     *
+     * @param builder                 the builder
+     * @param authorizedClientManager the authorized client manager
+     * @return the web client
+     * @see OAuth2AuthorizedClientManager
+     * @see AuthorizedClientServiceOAuth2AuthorizedClientManager
+     */
     @Bean
-    @ConditionalOnBean({WebClient.Builder.class, AuthorizedClientServiceOAuth2AuthorizedClientManager.class})
+    @ConditionalOnBean(value = {WebClient.Builder.class}, name = {"clientServiceAuthorizedClientManager"})
     WebClient nonServletWebClient(WebClient.Builder builder,
-                                  @Qualifier("clientServiceAuthorizedClientManager") OAuth2AuthorizedClientManager authorizedClientManager) {
+                                  @Qualifier("clientServiceAuthorizedClientManager")
+                                  OAuth2AuthorizedClientManager authorizedClientManager) {
         builder.filters(filters ->
                 filters.add(0, new ServletOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
                 ));
@@ -161,18 +185,31 @@ public class RestClientAutoConfiguration {
         return new RestAPIClient(webClient, "maat-api");
     }
 
+    /**
+     * Configures a RestApiClient bean for communicating with MaatApi in non-servlet environments
+     * (e.g. SQS listeners, scheduled tasks) if an OAuth2 configuration for MaatAPI is found and Spring Cloud
+     * access credentials are configured
+     *
+     * @param webClient the web client
+     * @return the rest api client
+     */
+    @Bean
+    @ConditionalOnProperty(name = {"spring.cloud.aws.credentials.access-key",
+            "spring.security.oauth2.client.provider.maat-api.token-uri"}
+    )
+    RestAPIClient maatApiNonServletClient(@Qualifier("nonServletWebClient") WebClient webClient) {
+        return new RestAPIClient(webClient, "maat-api");
+    }
+
+    /**
+     * Configures a RestApiClient bean if an OAuth2 configuration for the Evidence service is found
+     *
+     * @param webClient the web client
+     * @return the rest api client
+     */
     @Bean
     @ConditionalOnProperty(name = "spring.security.oauth2.client.provider.evidence.token-uri")
     RestAPIClient evidenceApiClient(WebClient webClient) {
         return new RestAPIClient(webClient, "evidence");
     }
-
-    @Bean
-    @ConditionalOnProperty("spring.security.oauth2.client.provider.maat-api.token-uri")
-    @ConditionalOnExpression("#{${feature.prosecution-concluded-schedule.enabled:false} or " +
-            "${feature.prosecution-concluded-listener.enabled:false}}")
-    RestAPIClient maatApiSchedulerClient(@Qualifier("nonServletWebClient") WebClient webClient) {
-        return new RestAPIClient(webClient, "maat-api");
-    }
-
 }
