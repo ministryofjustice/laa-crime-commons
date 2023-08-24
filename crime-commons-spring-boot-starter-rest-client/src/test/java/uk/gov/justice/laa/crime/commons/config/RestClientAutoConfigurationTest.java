0fca68bb4026262c9ad7b8bb75ef291082103967
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2023-08-24 17:37:52 +0100

    New autoconfiguration test scenarios

--- crime-commons-spring-boot-starter-rest-client/src/test/java/uk/gov/justice/laa/crime/commons/config/RestClientAutoConfigurationTest.java
+++ crime-commons-spring-boot-starter-rest-client/src/test/java/uk/gov/justice/laa/crime/commons/config/RestClientAutoConfigurationTest.java
@@ -1,5 +1,6 @@
 package uk.gov.justice.laa.crime.commons.config;
 
+import org.eclipse.jetty.util.ArrayUtil;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.autoconfigure.security.oauth2.client.servlet.OAuth2ClientAutoConfiguration;
@@ -13,11 +14,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServletOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
 import org.springframework.web.reactive.function.client.WebClient;
-import reactor.netty.resources.ConnectionProvider;
 
 import java.util.HashMap;
 import java.util.List;
@@ -33,8 +35,10 @@ class RestClientAutoConfigurationTest {
     private static final String EVIDENCE_REGISTRATION_ID = "evidence";
     private static final String CDA_API_CLIENT_BEAN = "cdaApiClient";
     private static final String MAAT_API_CLIENT_BEAN = "maatApiClient";
+    private static final String MAAT_API_NON_SERVLET_CLIENT_BEAN = "maatApiNonServletClient";
     private static final String EVIDENCE_API_CLIENT_BEAN = "evidenceApiClient";
     private static final String REGISTRATION_PREFIX = "spring.security.oauth2.client.registration";
+    private static final String SPRING_CLOUD_PREFIX = "spring.cloud.aws.credentials";
     private static final String REGISTRATION_KEY_NAME =
             "org.springframework.security.oauth2.client.OAuth2AuthorizedClient.CLIENT_REGISTRATION_ID";
     private static final String OAUTH_CLIENT_PROVIDER_PREFIX = "spring.security.oauth2.client.provider";
@@ -52,12 +56,7 @@ class RestClientAutoConfigurationTest {
                 .run((context) -> {
                     assertThat(context).hasSingleBean(WebClientCustomizer.class);
                     List<ExchangeFilterFunction> filters = getFilters(context);
-                    assertThat(filters).hasSize(5);
-                    assertThat(filters.stream()
-                            .anyMatch(exchangeFilterFunction ->
-                                    exchangeFilterFunction instanceof ServletOAuth2AuthorizedClientExchangeFilterFunction
-                            )
-                    ).isTrue();
+                    assertThat(filters).hasSize(4);
                     assertThat(getHeaders(context)).hasSize(2);
                     assertThat(getHeaders(context).getFirst(HttpHeaders.ACCEPT))
                             .isEqualTo(MediaType.APPLICATION_JSON_VALUE);
@@ -74,15 +73,59 @@ class RestClientAutoConfigurationTest {
     }
 
     @Test
-    void restApiClientConfigurerConfiguresCustomWebClientBean() {
+    void restApiClientConfigurerConfiguresDefaultWebClientBean() {
         this.contextRunner
                 .withUserConfiguration(TestConfig.class, WebClientAutoConfiguration.class)
                 .withPropertyValues(getOAuthPropertyValuesForClient(CDA_REGISTRATION_ID))
-                .run((context) -> assertThat(context).hasSingleBean(WebClient.class));
+                .run((context) -> {
+                    assertThat(context).hasSingleBean(WebClient.class);
+                    WebClient.Builder builder = (WebClient.Builder) ReflectionTestUtils.getField(
+                            context.getBean(WebClient.class), "builder"
+                    );
+                    List<ExchangeFilterFunction> filters = getFilters(builder);
+                    assertThat(filters.stream()
+                            .anyMatch(exchangeFilterFunction ->
+                                    exchangeFilterFunction instanceof ServletOAuth2AuthorizedClientExchangeFilterFunction
+                            )
+                    ).isTrue();
+                    assertThat(filters).hasSize(5);
+                });
     }
 
     @Test
-    void cdaApiClientIsConditionalOnOauthConfiguration() {
+    void restApiClientConfigurerConfiguresNonServletWebClientBean() {
+        this.contextRunner
+                .withUserConfiguration(TestConfig.class, WebClientAutoConfiguration.class)
+                .withPropertyValues(ArrayUtil.add(
+                                getSpringCloudPropertyValuesForClient(),
+                                getOAuthPropertyValuesForClient(MAAT_API_CLIENT_BEAN)
+                        )
+                )
+                .run((context) -> {
+                    Map<String, WebClient> webClientBeans = context.getBeansOfType(WebClient.class);
+                    assertThat(webClientBeans).hasSize(2);
+                    WebClient.Builder builder = (WebClient.Builder) ReflectionTestUtils.getField(
+                            webClientBeans.get("nonServletWebClient"), "builder"
+                    );
+                    List<ExchangeFilterFunction> filters = getFilters(builder);
+                    assertThat(filters).hasSize(5);
+
+                    ExchangeFilterFunction oAuthFilter = filters.stream()
+                            .filter(exchangeFilterFunction ->
+                                    exchangeFilterFunction instanceof ServletOAuth2AuthorizedClientExchangeFilterFunction
+                            ).findFirst().orElseThrow();
+
+                    OAuth2AuthorizedClientManager authorizedClientManager =
+                            (OAuth2AuthorizedClientManager) ReflectionTestUtils.getField(
+                                    oAuthFilter, "authorizedClientManager"
+                            );
+                    assertThat(authorizedClientManager)
+                            .isInstanceOf(AuthorizedClientServiceOAuth2AuthorizedClientManager.class);
+                });
+    }
+
+    @Test
+    void cdaApiClientIsConditionalOnOAuthConfiguration() {
         this.contextRunner
                 .withUserConfiguration(TestConfig.class, WebClientAutoConfiguration.class)
                 .withPropertyValues(getOAuthPropertyValuesForClient(CDA_REGISTRATION_ID))
@@ -99,7 +142,7 @@ class RestClientAutoConfigurationTest {
     }
 
     @Test
-    void maatApiClientIsConditionalOnOauthConfiguration() {
+    void maatApiClientIsConditionalOnOAuthConfiguration() {
         this.contextRunner
                 .withUserConfiguration(TestConfig.class, WebClientAutoConfiguration.class)
                 .withPropertyValues(getOAuthPropertyValuesForClient(MAAT_API_REGISTRATION_ID))
@@ -116,7 +159,25 @@ class RestClientAutoConfigurationTest {
     }
 
     @Test
-    void evidenceApiClientIsConditionalOnOauthConfiguration() {
+    void maatApiNonServletClientIsConditionalOnOAuthConfigurationAndNonServletWebClient() {
+        this.contextRunner
+                .withUserConfiguration(TestConfig.class, WebClientAutoConfiguration.class)
+                .withPropertyValues(getOAuthPropertyValuesForClient(MAAT_API_REGISTRATION_ID))
+                .run((context) -> assertThat(context).doesNotHaveBean(MAAT_API_NON_SERVLET_CLIENT_BEAN));
+    }
+
+    @Test
+    void restApiClientConfigurerConfiguresMaatApiNonServletClient() {
+        this.contextRunner
+                .withUserConfiguration(TestConfig.class, WebClientAutoConfiguration.class)
+                .withPropertyValues(getSpringCloudPropertyValuesForClient())
+                .withPropertyValues(getOAuthPropertyValuesForClient(MAAT_API_REGISTRATION_ID))
+                .withPropertyValues(OAUTH_CLIENT_PROVIDER_PREFIX + ".maat-api.token-uri=mock-url")
+                .run((context) -> assertThat(context).hasBean(MAAT_API_NON_SERVLET_CLIENT_BEAN));
+    }
+
+    @Test
+    void evidenceApiClientIsConditionalOnOAuthConfiguration() {
         this.contextRunner
                 .withUserConfiguration(TestConfig.class, WebClientAutoConfiguration.class)
                 .withPropertyValues(getOAuthPropertyValuesForClient(EVIDENCE_REGISTRATION_ID))
@@ -158,9 +219,22 @@ class RestClientAutoConfigurationTest {
         };
     }
 
+    private String[] getSpringCloudPropertyValuesForClient() {
+        return new String[]{
+                SPRING_CLOUD_PREFIX + ".access-key=abc",
+                SPRING_CLOUD_PREFIX + ".secret-key=secret",
+                SPRING_CLOUD_PREFIX + ".region.static=eu-west2"
+        };
+    }
+
     @SuppressWarnings("unchecked")
     private List<ExchangeFilterFunction> getFilters(AssertableWebApplicationContext context) {
         WebClient.Builder builder = context.getBean(WebClient.Builder.class);
+        return (List<ExchangeFilterFunction>) ReflectionTestUtils.getField(builder, "filters");
+    }
+
+    @SuppressWarnings("unchecked")
+    private List<ExchangeFilterFunction> getFilters(WebClient.Builder builder) {
         return (List<ExchangeFilterFunction>) ReflectionTestUtils.getField(builder, "filters");
     }
 
