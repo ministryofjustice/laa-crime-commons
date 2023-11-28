21ea899ddc097b1257091e9c071af456c93c563b
muthusubra <muthu.subramanian@digital.justice.gov.uk>
2023-11-28 15:54:30 +0000

    Remove test cases

--- crime-commons-spring-boot-starter-rest-client/src/test/java/uk/gov/justice/laa/crime/commons/config/TracerAutoConfigurationTest.java
+++ crime-commons-spring-boot-starter-rest-client/src/test/java/uk/gov/justice/laa/crime/commons/config/TracerAutoConfigurationTest.java
@@ -2,11 +2,9 @@ package uk.gov.justice.laa.crime.commons.config;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.actuate.autoconfigure.tracing.BraveAutoConfiguration;
-import org.springframework.boot.actuate.autoconfigure.tracing.zipkin.ZipkinAutoConfiguration;
 import org.springframework.boot.autoconfigure.AutoConfigurations;
 import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
 import uk.gov.justice.laa.crime.commons.tracing.TraceIdHandler;
-import zipkin2.reporter.AsyncReporter;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -27,20 +25,4 @@ class TracerAutoConfigurationTest {
                 .withUserConfiguration(BraveAutoConfiguration.class)
                 .run((context) -> assertThat(context).hasSingleBean(TraceIdHandler.class));
     }
-
-    @Test
-    void asyncReporterIsConditionalOnZipkinConfiguration() {
-        this.contextRunner
-                .run((context) -> assertThat(context).
-                        doesNotHaveBean(AsyncReporter.class));
-    }
-
-    @Test
-    void micrometerZipkinAutoConfigurerConfiguresAsyncReporter() {
-        this.contextRunner
-                .withUserConfiguration(ZipkinAutoConfiguration.class)
-                .run((context) -> assertThat(context).
-                        hasSingleBean(AsyncReporter.class));
-    }
-
 }
