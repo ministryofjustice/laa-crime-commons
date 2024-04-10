9f4e5d6255c4abed7c7638a7fc2899a8e8ad7640
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2024-04-10 16:30:02 +0100

    LCAM-1287: Added unit tests to FileUtils

--- crime-commons-classes/src/test/java/uk/gov/justice/laa/crime/util/FileUtilsTest.java
+++ crime-commons-classes/src/test/java/uk/gov/justice/laa/crime/util/FileUtilsTest.java
@@ -1,10 +1,20 @@
 package uk.gov.justice.laa.crime.util;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.MockedStatic;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.io.File;
+import java.io.UTFDataFormatException;
+import java.nio.charset.Charset;
 
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.ArgumentMatchers.any;
 
+@ExtendWith(MockitoExtension.class)
 public class FileUtilsTest {
 
     @Test
@@ -15,6 +25,14 @@ public class FileUtilsTest {
     @Test
     void verifyThatRuntimeExceptionIsThrownWhenSpecifiedFileDoesNotExist() {
         assertThrows(RuntimeException.class, () -> FileUtils.readFileToString("data/nofileexists.json"));
+    }
+
+    @Test
+    void verifyThatRunTimeExceptionIsThrownOnValidIOException() {
+        try(MockedStatic<org.apache.commons.io.FileUtils> fileUtils = Mockito.mockStatic(org.apache.commons.io.FileUtils.class)) {
+            fileUtils.when(() -> org.apache.commons.io.FileUtils.readFileToString(any(File.class), any(Charset.class))).thenThrow(UTFDataFormatException.class);
+            assertThrows(RuntimeException.class, () -> FileUtils.readFileToString("data/reporder_dto.json"));
+        }
     }
 
 }
