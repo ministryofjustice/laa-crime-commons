3de6b37992fd05e8f733d5ee506c7500040f18a1
Sriram Kasthuri <sriram.kasthuri@digital.justice.gov.uk>
2024-04-18 15:20:42 +0100

    LCAM-1314: Refactored to ApplicationRunner to enable automatic context propagation on startup.

--- crime-commons-classes/src/main/java/uk/gov/justice/laa/crime/config/ContextPropagationInitializingSingleton.java
+++ crime-commons-classes/src/main/java/uk/gov/justice/laa/crime/config/ContextPropagationInitializingSingleton.java
@@ -1,18 +1,23 @@
 package uk.gov.justice.laa.crime.config;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.SmartInitializingSingleton;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Hooks;
 
 /**
  * Automatically wrap Reactor internal mechanisms to propagate context between operators, threads etc. on application startup.
  */
+@Order(2)
 @Component
 @Slf4j
-public class ContextPropagationInitializingSingleton implements SmartInitializingSingleton {
+public class ContextPropagationInitializingSingleton implements ApplicationRunner {
+
     @Override
-    public void afterSingletonsInstantiated() {
+    public void run(ApplicationArguments applicationArguments) throws Exception {
+        log.info("********** Context Propagation Initializer **************");
         Hooks.enableAutomaticContextPropagation();
     }
 }
