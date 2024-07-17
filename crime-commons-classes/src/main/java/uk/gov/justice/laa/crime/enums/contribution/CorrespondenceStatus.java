da988cfc7ef5f2003e61c671a1bd802b88b08cc0
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2024-07-17 14:41:37 +0100

    New CorrespondenceStatus enum & associated tests

---
+++ crime-commons-classes/src/main/java/uk/gov/justice/laa/crime/enums/contribution/CorrespondenceStatus.java
@@ -0,0 +1,34 @@
+package uk.gov.justice.laa.crime.enums.contribution;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.stream.Stream;
+
+@Getter
+@AllArgsConstructor
+public enum CorrespondenceStatus {
+    APPEAL_CC("appealCC"),
+    CDS15("cds15"),
+    REASS("re-ass"),
+    NONE("none");
+
+    private static final String EXCEPTION_MESSAGE = "Correspondence status with value: %s does not exist";
+
+    @NotNull
+    private final String status;
+
+    public static CorrespondenceStatus getFrom(String status) {
+        if (StringUtils.isBlank(status)) {
+            return null;
+        }
+
+        return Stream.of(CorrespondenceStatus.values())
+                .filter(currentStatus -> currentStatus.status.equals(status))
+                .findFirst()
+                .orElseThrow(() -> new IllegalArgumentException(String.format(EXCEPTION_MESSAGE, status)));
+    }
+
+}
