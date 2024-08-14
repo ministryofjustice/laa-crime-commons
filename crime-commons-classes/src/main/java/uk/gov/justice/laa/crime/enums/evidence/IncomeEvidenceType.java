851d08e07dde11224452247d7e80341b85468492
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2024-08-14 15:56:23 +0100

    Introduce more constants in the IncomeEvidenceType class
    This will help to cut down on duplication.

--- crime-commons-classes/src/main/java/uk/gov/justice/laa/crime/enums/evidence/IncomeEvidenceType.java
+++ crime-commons-classes/src/main/java/uk/gov/justice/laa/crime/enums/evidence/IncomeEvidenceType.java
@@ -12,21 +12,21 @@ import java.util.stream.Stream;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public enum IncomeEvidenceType {
 
-    SIGNATURE("SIGNATURE", "Signature", "Signature", "Llofnod", false),
+    SIGNATURE("SIGNATURE", Constants.SIGNATURE, Constants.SIGNATURE, "Llofnod", false),
     CDS15("CDS15", Constants.CDS_15, Constants.CDS_15, Constants.CDS_15, true),
-    FREEZING("FREEZING", "Freezing order", "Freezing order", "Gorchymyn Rhewi", true),
-    RESTRAINING("RESTRAINING", "Restraint Order", "Restraint Order", "Gorchymyn Atal", true),
-    CONFISCATION("CONFISCATION", "Confiscation order", "Confiscation order", "Gorchymyn Atafaeliad", true),
-    OTHER_ADHOC("OTHER_ADHOC", "Other Adhoc", "Other Adhoc", "Ad Hoc eraill", true),
+    FREEZING("FREEZING", Constants.FREEZING_ORDER, Constants.FREEZING_ORDER, "Gorchymyn Rhewi", true),
+    RESTRAINING("RESTRAINING", Constants.RESTRAINT_ORDER, Constants.RESTRAINT_ORDER, "Gorchymyn Atal", true),
+    CONFISCATION("CONFISCATION", Constants.CONFISCATION_ORDER, Constants.CONFISCATION_ORDER, "Gorchymyn Atafaeliad", true),
+    OTHER_ADHOC("OTHER_ADHOC", Constants.OTHER_ADHOC, Constants.OTHER_ADHOC, "Ad Hoc eraill", true),
     EMP_LETTER_ADHOC("EMP LETTER ADHOC", Constants.LETTER_FROM_EMPLOYER, Constants.LETTER_FROM_EMPLOYER, "Llythyr o'r cyflogwr", true),
     WAGE_SLIP_ADHOC("WAGE SLIP ADHOC", "Wage Slip", "Wage Slip within past 3 months", "Papur Cyflog o fewn y tri mis diwethaf", true),
-    NINO("NINO", "National Insurance Number", "National Insurance Number", "Rhif Yswiriant Cenedlaethol", false),
-    ACCOUNTS("ACCOUNTS", "Set of Accounts", "Set of Accounts", "Cyfrifon", false),
-    OTHER_BUSINESS("OTHER BUSINESS", "Other Business Records", "Other Business Records", "Cofnodion Busnes eraill", false),
-    CASH_BOOK("CASH BOOK", "Cash Book", "Cash Book", "Llyfr Arian", false),
+    NINO("NINO", Constants.NATIONAL_INSURANCE_NUMBER, Constants.NATIONAL_INSURANCE_NUMBER, "Rhif Yswiriant Cenedlaethol", false),
+    ACCOUNTS("ACCOUNTS", Constants.SET_OF_ACCOUNTS, Constants.SET_OF_ACCOUNTS, "Cyfrifon", false),
+    OTHER_BUSINESS("OTHER BUSINESS", Constants.OTHER_BUSINESS_RECORDS, Constants.OTHER_BUSINESS_RECORDS, "Cofnodion Busnes eraill", false),
+    CASH_BOOK("CASH BOOK", Constants.CASH_BOOK, Constants.CASH_BOOK, "Llyfr Arian", false),
     WAGE_SLIP("WAGE SLIP", "Wage Slip", "Wage Slip within past 3 months", "Papur Cyflog o fewn y tri mis diwethaf", false),
     BANK_STATEMENT("BANK STATEMENT", "Bank Statement", "Bank Statement(s) covering 3 months", "Cyfriflen Banc", false),
-    TAX_RETURN("TAX RETURN", "Tax Return", "Tax Return", "Ffurflen Dreth", false),
+    TAX_RETURN("TAX RETURN", Constants.TAX_RETURN, Constants.TAX_RETURN, "Ffurflen Dreth", false),
     EMP_LETTER("EMP LETTER", Constants.LETTER_FROM_EMPLOYER, Constants.LETTER_FROM_EMPLOYER, "Llythyr oddi wrth Gyflogwr", false),
     OTHER("OTHER", "Other Ad-hoc evidence", "Text to be entered", "", false);
 
@@ -51,5 +51,15 @@ public enum IncomeEvidenceType {
     private static class Constants {
         public static final String CDS_15 = "CDS 15";
         public static final String LETTER_FROM_EMPLOYER = "Letter from Employer";
+        public static final String OTHER_ADHOC = "Other Adhoc";
+        public static final String NATIONAL_INSURANCE_NUMBER = "National Insurance Number";
+        public static final String CASH_BOOK = "Cash Book";
+        public static final String TAX_RETURN = "Tax Return";
+        public static final String OTHER_BUSINESS_RECORDS = "Other Business Records";
+        public static final String SET_OF_ACCOUNTS = "Set of Accounts";
+        public static final String CONFISCATION_ORDER = "Confiscation order";
+        public static final String RESTRAINT_ORDER = "Restraint Order";
+        public static final String FREEZING_ORDER = "Freezing order";
+        public static final String SIGNATURE = "Signature";
     }
 }
