4ca29267bcd44f4d6002f2657f9fc546326f9fae
Venkat Velpuri <venkat.velpuri@digital.justice.gov.uk>
2024-11-01 16:39:14 +0000

    Adding tracing auto configuration

---
+++ crime-commons-classes/src/main/java/uk/gov/justice/laa/crime/config/TracingAutoConfiguration.java
@@ -0,0 +1,36 @@
+package uk.gov.justice.laa.crime.config;
+
+import io.micrometer.tracing.Tracer;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import uk.gov.justice.laa.crime.tracing.TraceIdHandler;
+
+
+/**
+ * Autoconfigures <Code>TraceIdHandler</Code> bean for REST API Exception Handler and exclude <Code>ZipkinAutoConfiguration</Code>
+ */
+@Slf4j
+@Configuration
+@AutoConfiguration
+@RequiredArgsConstructor
+@PropertySource("classpath:application.properties")
+public class TracingAutoConfiguration {
+
+    /**
+     * Configures a <code>TraceIdHandler</code> bean for REST API Exception Handler
+     *
+     * @param tracer The underlying tracer to use
+     * @return TraceIdHandler
+     */
+    @Bean
+    @ConditionalOnBean(Tracer.class)
+    TraceIdHandler traceIdHandler(Tracer tracer) {
+        return new TraceIdHandler(tracer);
+    }
+
+}
---
+++ crime-commons-classes/src/test/java/uk/gov/justice/laa/crime/config/TracerAutoConfigurationTest.java
@@ -0,0 +1,28 @@
+package uk.gov.justice.laa.crime.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.actuate.autoconfigure.tracing.BraveAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigurations;
+import org.springframework.boot.test.context.runner.WebApplicationContextRunner;
+import uk.gov.justice.laa.crime.tracing.TraceIdHandler;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class TracerAutoConfigurationTest {
+
+    private final WebApplicationContextRunner contextRunner = new WebApplicationContextRunner()
+            .withConfiguration(AutoConfigurations.of(TracingAutoConfiguration.class));
+
+    @Test
+    void traceIdHandlerIsConditionalOnTracerBean() {
+        this.contextRunner
+                .run((context) -> assertThat(context).doesNotHaveBean(TraceIdHandler.class));
+    }
+
+    @Test
+    void traceConfigurerConfiguresTraceIdHandler() {
+        this.contextRunner
+                .withUserConfiguration(BraveAutoConfiguration.class)
+                .run((context) -> assertThat(context).hasSingleBean(TraceIdHandler.class));
+    }
+}
