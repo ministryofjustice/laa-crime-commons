bc32825025aa6b9af1476fd6a5b04bf5baf319c5
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2026-01-20 13:07:06 +0000

    Refactor ProblemDetailUtil for improved readability
    Reformatted method parameters and annotations for better alignment and consistency. Fixed minor spacing and indentation issues to enhance code readability and maintain coding standards.

--- crime-commons-classes/src/main/java/uk/gov/justice/laa/crime/util/ProblemDetailUtil.java
+++ crime-commons-classes/src/main/java/uk/gov/justice/laa/crime/util/ProblemDetailUtil.java
@@ -1,15 +1,14 @@
 package uk.gov.justice.laa.crime.util;
 
+import java.net.URI;
+import java.util.List;
+import java.util.Optional;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ProblemDetail;
 import uk.gov.justice.laa.crime.exception.ErrorExtension;
 import uk.gov.justice.laa.crime.exception.ErrorMessage;
-
-import java.net.URI;
-import java.util.List;
-import java.util.Optional;
 
 /**
  * Utility Class for single source for handling errors being passed via the ProblemDetail class.
@@ -21,12 +20,18 @@ public final class ProblemDetailUtil {
 
     /**
      * Creates a ProblemDetail object with minimal standard objects.
-     * @param status HttpStatus Http Error encountered, as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-status">RFC-9457 - status</a>
-     * @param detail String Details the error encountered, as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-detail">RFC-9457 - detail</a>
+     *
+     * @param status    HttpStatus Http Error encountered, as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-status">RFC-9457 -
+     *                  status</a>
+     * @param detail    String Details the error encountered, as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-detail">RFC-9457 -
+     *                  detail</a>
      * @param extension ErrorExtension holding additional details of the error.
      * @return ProblemDetail populated with given details
      */
-    public static ProblemDetail buildProblemDetail(HttpStatus status, String detail, ErrorExtension extension) {
+    public static ProblemDetail buildProblemDetail(HttpStatus status, String detail,
+            ErrorExtension extension) {
         ProblemDetail problemDetail = ProblemDetail.forStatusAndDetail(status, detail);
         problemDetail.setProperty(ErrorExtension.EXTENSION_KEY, extension);
         return problemDetail;
@@ -35,15 +40,25 @@ public final class ProblemDetailUtil {
     /**
      * Creates a ProblemDetail object for the given status and parameters.
      *
-     * @param status    String as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-status">RFC-9457 - status</a>
-     * @param title     String as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-title">RFC-9457 - title</a>
-     * @param type      URI as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-type">RFC-9457 - type</a>
-     * @param detail    String as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-detail">RFC-9457 - detail</a>
-     * @param instance  URI as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-instance">RFC-9457 - instance</a>
+     * @param status    String as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-status">RFC-9457 -
+     *                  status</a>
+     * @param title     String as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-title">RFC-9457 -
+     *                  title</a>
+     * @param type      URI as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-type">RFC-9457 - type</a>
+     * @param detail    String as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-detail">RFC-9457 -
+     *                  detail</a>
+     * @param instance  URI as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-instance">RFC-9457 -
+     *                  instance</a>
      * @param extension ErrorExtension holding additional details of the error.
      * @return ProblemDetail populated with given values.
      */
-    public static ProblemDetail buildProblemDetail(HttpStatus status, String title, URI type, String detail, URI instance, ErrorExtension extension) {
+    public static ProblemDetail buildProblemDetail(HttpStatus status, String title, URI type,
+            String detail, URI instance, ErrorExtension extension) {
         ProblemDetail problemDetail = buildProblemDetail(status, detail, extension);
         problemDetail.setInstance(instance);
         problemDetail.setTitle(title);
@@ -53,28 +68,42 @@ public final class ProblemDetailUtil {
 
     /**
      * Returns a ProblemDetail object for the given status and String parameters.
-     * @param status    String as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-status">RFC-9457 - status</a>
-     * @param title     String as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-title">RFC-9457 - title</a>
-     * @param type      URI String as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-type">RFC-9457 - type</a>
-     * @param detail    String as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-detail">RFC-9457 - detail</a>
-     * @param instance  URI String as per <a href="https://www.rfc-editor.org/rfc/rfc9457#name-instance">RFC-9457 - instance</a>
+     *
+     * @param status    String as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-status">RFC-9457 -
+     *                  status</a>
+     * @param title     String as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-title">RFC-9457 -
+     *                  title</a>
+     * @param type      URI String as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-type">RFC-9457 - type</a>
+     * @param detail    String as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-detail">RFC-9457 -
+     *                  detail</a>
+     * @param instance  URI String as per <a
+     *                  href="https://www.rfc-editor.org/rfc/rfc9457#name-instance">RFC-9457 -
+     *                  instance</a>
      * @param extension ErrorExtension for holding additional details of the error.
      */
-    public static ProblemDetail buildProblemDetail(HttpStatus status, String title, String type, String detail, String instance, ErrorExtension extension) {
-        return buildProblemDetail(status, title, URI.create(type), detail, URI.create(instance), extension);
+    public static ProblemDetail buildProblemDetail(HttpStatus status, String title, String type,
+            String detail, String instance, ErrorExtension extension) {
+        return buildProblemDetail(status, title, URI.create(type), detail, URI.create(instance),
+                extension);
     }
 
     /**
      * Builds an ErrorExtension from given details to be used with a ProblemDetail
-     * @param code String for additional identification of the error.
+     *
+     * @param code    String for additional identification of the error.
      * @param traceId TraceId of the transaction.
-     * @param errors {@literal List<ErrorMessage>} of problems encountered during transaction.
+     * @param errors  {@literal List<ErrorMessage>} of problems encountered during transaction.
      */
-    public static ErrorExtension buildErrorExtension( String code, String traceId, List<ErrorMessage> errors ) {
-        return new ErrorExtension( code, traceId, errors);
+    public static ErrorExtension buildErrorExtension(String code, String traceId,
+            List<ErrorMessage> errors) {
+        return new ErrorExtension(code, traceId, errors);
     }
 
-    public static Optional<ErrorExtension> getErrorExtension(ProblemDetail problemDetail){
+    public static Optional<ErrorExtension> getErrorExtension(ProblemDetail problemDetail) {
         return Optional.ofNullable(problemDetail)
                 .map(ProblemDetail::getProperties)
                 .map(properties -> properties.get(ErrorExtension.EXTENSION_KEY))
@@ -90,7 +119,7 @@ public final class ProblemDetailUtil {
      * <li>A list containing the detail field of the {@code ProblemDetail}</li>
      * <li>An empty list if neither are set.</li></ul>
      */
-    public static List<String> getErrorDetails(ProblemDetail problemDetail){
+    public static List<String> getErrorDetails(ProblemDetail problemDetail) {
         return getErrorMessages(problemDetail).stream().map(ErrorMessage::getMessage).toList();
     }
 
@@ -107,10 +136,10 @@ public final class ProblemDetailUtil {
                 .map(ErrorExtension::getErrors)
                 .orElseGet(() ->
                         Optional.ofNullable(problemDetail)
-                        .map(ProblemDetail::getDetail)
-                        .map(detail -> new ErrorMessage(FALLBACK_DETAIL_FIELD_NAME, detail))
-                        .map(List::of)
-                        .orElseGet(List::of));
+                                .map(ProblemDetail::getDetail)
+                                .map(detail -> new ErrorMessage(FALLBACK_DETAIL_FIELD_NAME, detail))
+                                .map(List::of)
+                                .orElseGet(List::of));
     }
 
 }
