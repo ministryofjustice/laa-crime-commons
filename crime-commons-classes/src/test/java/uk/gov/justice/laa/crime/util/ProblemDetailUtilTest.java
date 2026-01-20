3c9711f9760edc1f7c54debc4a3ba3e20f1cb92f
Matthew Howell <matthew.howell@digital.justice.gov.uk>
2026-01-20 13:48:52 +0000

    Refactor ProblemDetailUtilTest for code readability
    Improved code formatting, fixed method and variable spacing, and aligned style for consistency. This enhances readability and adheres to coding standards without modifying core functionality.

--- crime-commons-classes/src/test/java/uk/gov/justice/laa/crime/util/ProblemDetailUtilTest.java
+++ crime-commons-classes/src/test/java/uk/gov/justice/laa/crime/util/ProblemDetailUtilTest.java
@@ -1,10 +1,6 @@
 package uk.gov.justice.laa.crime.util;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ProblemDetail;
-import uk.gov.justice.laa.crime.exception.ErrorExtension;
-import uk.gov.justice.laa.crime.exception.ErrorMessage;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
 import java.net.URI;
 import java.util.ArrayList;
@@ -12,15 +8,19 @@ import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 import java.util.stream.IntStream;
-
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ProblemDetail;
+import uk.gov.justice.laa.crime.exception.ErrorExtension;
+import uk.gov.justice.laa.crime.exception.ErrorMessage;
 
 class ProblemDetailUtilTest {
 
     @Test
-    void givenErrorList_whenErrorResponseWanted_ErrorListPresent(){
+    void givenErrorList_whenErrorResponseWanted_ErrorListPresent() {
         ErrorExtension extension = createErrorExtension(2);
-        ProblemDetail problemDetail = ProblemDetailUtil.buildProblemDetail(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.getReasonPhrase(), extension);
+        ProblemDetail problemDetail = ProblemDetailUtil.buildProblemDetail(HttpStatus.BAD_REQUEST,
+                HttpStatus.BAD_REQUEST.getReasonPhrase(), extension);
         assertThat(problemDetail).isNotNull();
         assertThat(ProblemDetailUtil.getErrorExtension(problemDetail))
                 .contains(extension);
@@ -28,7 +28,7 @@ class ProblemDetailUtilTest {
 
     // Test without using utility constructor. Verify getter behaviour.
     @Test
-    void givenErrorListPresent_whenProblemDetailPassedIn_thenSameErrorListReturned(){
+    void givenErrorListPresent_whenProblemDetailPassedIn_thenSameErrorListReturned() {
         ErrorExtension extension = createErrorExtension(2);
         ProblemDetail problemDetail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST.value());
         problemDetail.setProperties(Map.of(ErrorExtension.EXTENSION_KEY, extension));
@@ -38,22 +38,24 @@ class ProblemDetailUtilTest {
     }
 
     @Test
-    void givenErrorList_whenProblemDetailCreatedAndThenPassedBack_thenSameErrorsPresent(){
+    void givenErrorList_whenProblemDetailCreatedAndThenPassedBack_thenSameErrorsPresent() {
         ErrorExtension extension = createErrorExtension(3);
-        ProblemDetail problemDetail = ProblemDetailUtil.buildProblemDetail(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.getReasonPhrase(), extension);
+        ProblemDetail problemDetail = ProblemDetailUtil.buildProblemDetail(HttpStatus.BAD_REQUEST,
+                HttpStatus.BAD_REQUEST.getReasonPhrase(), extension);
         List<String> foundErrors = ProblemDetailUtil.getErrorDetails(problemDetail);
         assertThat(foundErrors).isEqualTo(getErrorsFromExtension(extension));
     }
 
     @Test
-    void givenNoListButHasDetail_whenProblemDetailExamined_thenListOfDetailReturned(){
-        ProblemDetail detail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, HttpStatus.BAD_REQUEST.getReasonPhrase());
+    void givenNoListButHasDetail_whenProblemDetailExamined_thenListOfDetailReturned() {
+        ProblemDetail detail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                HttpStatus.BAD_REQUEST.getReasonPhrase());
         List<String> foundErrors = ProblemDetailUtil.getErrorDetails(detail);
         assertThat(foundErrors).isEqualTo(List.of(HttpStatus.BAD_REQUEST.getReasonPhrase()));
     }
 
     @Test
-    void givenNoDetailsOrList_whenProblemDetailsExamined_thenEmptyListReturned(){
+    void givenNoDetailsOrList_whenProblemDetailsExamined_thenEmptyListReturned() {
         ProblemDetail detail = ProblemDetail.forStatus(HttpStatus.BAD_REQUEST);
         List<String> foundErrors = ProblemDetailUtil.getErrorDetails(detail);
         assertThat(foundErrors).isEqualTo(List.of());
@@ -61,35 +63,38 @@ class ProblemDetailUtilTest {
 
 
     @Test
-    void givenListAndDetail_whenGetListIsCalled_thenOnlyListReturned(){
+    void givenListAndDetail_whenGetListIsCalled_thenOnlyListReturned() {
         ErrorExtension extension = createErrorExtension(2);
-        ProblemDetail problemDetail = ProblemDetailUtil.buildProblemDetail(HttpStatus.BAD_REQUEST, "detail", extension);
+        ProblemDetail problemDetail = ProblemDetailUtil.buildProblemDetail(HttpStatus.BAD_REQUEST,
+                "detail", extension);
 
         List<String> foundErrors = ProblemDetailUtil.getErrorDetails(problemDetail);
         assertThat(foundErrors).isEqualTo(getErrorsFromExtension(extension));
     }
 
     @Test
-    void givenListPresentButEmpty_whenProblemDetailPassedIn_thenListOfDetailReturned(){
+    void givenListPresentButEmpty_whenProblemDetailPassedIn_thenListOfDetailReturned() {
         ErrorExtension extension = new ErrorExtension("code", "trace", List.of());
-        ProblemDetail detail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST, "Test Detail To Be Found");
+        ProblemDetail detail = ProblemDetail.forStatusAndDetail(HttpStatus.BAD_REQUEST,
+                "Test Detail To Be Found");
         detail.setProperties(Map.of(ErrorExtension.EXTENSION_KEY, extension));
         List<String> foundErrors = ProblemDetailUtil.getErrorDetails(detail);
         assertThat(foundErrors).isEqualTo(List.of("Test Detail To Be Found"));
     }
 
     @Test
-    void givenNoProblemDetail_whenNullPassedIn_thenEmptyListReturned(){
+    void givenNoProblemDetail_whenNullPassedIn_thenEmptyListReturned() {
         List<String> foundErrors = ProblemDetailUtil.getErrorDetails(null);
         assertThat(foundErrors).isEqualTo(List.of());
     }
 
     @Test
-    void givenAllDetails_whenBuildCalled_thenFullDetailReturned(){
+    void givenAllDetails_whenBuildCalled_thenFullDetailReturned() {
 
         ErrorExtension expectedExtension = createErrorExtension(2);
         ProblemDetail problemDetail =
-                ProblemDetailUtil.buildProblemDetail(HttpStatus.BAD_REQUEST, "title", "type", "detail", "instance", expectedExtension);
+                ProblemDetailUtil.buildProblemDetail(HttpStatus.BAD_REQUEST, "title", "type",
+                        "detail", "instance", expectedExtension);
 
         assertThat(problemDetail.getTitle()).isEqualTo("title");
         assertThat(problemDetail.getType()).isEqualTo(URI.create("type"));
@@ -97,31 +102,34 @@ class ProblemDetailUtilTest {
         assertThat(problemDetail.getInstance()).isEqualTo(URI.create("instance"));
         assertThat(problemDetail.getStatus()).isEqualTo(HttpStatus.BAD_REQUEST.value());
 
-        Optional<ErrorExtension> foundExtension = ProblemDetailUtil.getErrorExtension(problemDetail);
+        Optional<ErrorExtension> foundExtension = ProblemDetailUtil.getErrorExtension(
+                problemDetail);
         assertThat(foundExtension).isPresent();
         assertThat(foundExtension).contains(expectedExtension);
     }
 
     @Test
-    void givenExtensionDetails_whenBuildExtensionCalled_populatesCorrectly(){
+    void givenExtensionDetails_whenBuildExtensionCalled_populatesCorrectly() {
         ErrorExtension expectedExtension = createErrorExtension(2);
 
-        ErrorExtension actualExtension = ProblemDetailUtil.buildErrorExtension(expectedExtension.code(), expectedExtension.traceId(), expectedExtension.errors());
+        ErrorExtension actualExtension = ProblemDetailUtil.buildErrorExtension(
+                expectedExtension.code(), expectedExtension.traceId(), expectedExtension.errors());
 
         assertThat(actualExtension).isEqualTo(expectedExtension);
     }
 
-    private List<ErrorMessage> createErrorMessages(int numValues){
+    private List<ErrorMessage> createErrorMessages(int numValues) {
         List<ErrorMessage> errors = new ArrayList<>();
-        IntStream.range(0, numValues).forEach(i -> errors.add(new ErrorMessage("Field"+i, "Test Message"+i)));
+        IntStream.range(0, numValues)
+                .forEach(i -> errors.add(new ErrorMessage("Field" + i, "Test Message" + i)));
         return errors;
     }
 
-    private ErrorExtension createErrorExtension(int numValues){
+    private ErrorExtension createErrorExtension(int numValues) {
         return new ErrorExtension("TestCode", "Test Trace ID", createErrorMessages(numValues));
     }
 
-    private List<String> getErrorsFromExtension(ErrorExtension extension){
+    private List<String> getErrorsFromExtension(ErrorExtension extension) {
         return extension.errors().stream().map(ErrorMessage::message).toList();
     }
 }
